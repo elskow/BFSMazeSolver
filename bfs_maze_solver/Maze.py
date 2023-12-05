d7c714b38fe2aaabd5244c4338dcb654ed97d9b4
elskow <elskow@outlook.com>
2023-12-05 15:08:16 +0700

    feat: add border to the formatter  @NaufalF121

--- bfs_maze_solver/Maze.py
+++ bfs_maze_solver/Maze.py
@@ -4,10 +4,20 @@ import numpy as np
 class Maze:
     """Represents the maze."""
 
-    def __init__(self, maze_str):
+    def __init__(self, maze_str, border: bool = True):
         self.maze = np.array(
             [list(map(int, x.strip().split(" "))) for x in maze_str.split("\n") if x]
         )
+        if border:
+            maze = np.array(
+                [
+                    list(map(int, x.strip().split(" ")))
+                    for x in maze_str.split("\n")
+                    if x
+                ]
+            )
+            self.maze = self.add_border(maze)
+
         self.height, self.width = self.shape = self.maze.shape
 
     def get_val(self, x, y):
@@ -15,3 +25,14 @@ class Maze:
 
     def set_value(self, x, y, val):
         self.maze[x, y] = val
+
+    def add_border(self, maze):
+        x = 1
+        y = len(maze)
+        maze[0] = [1 for i in maze[0]]
+        maze[-1] = [1 for i in maze[-1]]
+        for i in maze:
+            i[0] = 1
+            i[-1] = 1
+
+        return maze
--- bfs_maze_solver/MazeFormatter.py
+++ bfs_maze_solver/MazeFormatter.py
@@ -1,27 +1,26 @@
-import os
 import cv2
+import os
 import numpy as np
 
-IMG_PATH = os.path.join(os.path.dirname(__file__), "../example/maze0.jpg")
+IMG_PATH = os.path.join(os.path.dirname(__file__), "../example/maze2.jpg")
 N = 33
 M = 15
 
 
 class MazeFormatter:
-    def __init__(self, img_path, n=33, m=15):
+    def __init__(self, img_path: str, N: int = 33, m: int = 15):
         self.img_path = img_path
         self.img = self.load_and_format_image(img_path)
-        self.N = n
+        self.N = N
         self.m = m
 
-    @staticmethod
-    def load_and_format_image(img_path):
+    def load_and_format_image(self, img_path: str):
         img = cv2.imread(img_path, 0)
         img = cv2.resize(img, (495, 495))
         _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return img
 
-    def convert(self):
+    def convert(self) -> str:
         (
             maze,
             white_cells,
@@ -33,7 +32,7 @@ class MazeFormatter:
         maze = self.adjust_maze_colors(maze, white_cells, black_cells, wall_thickness)
         return maze
 
-    def calculate_cells_and_walls(self):
+    def calculate_cells_and_walls(self) -> (str, int, int, int, int):
         maze = ""
         white_cells = black_cells = thick_walls = thin_walls = 0
         for i in range(self.N):
@@ -50,7 +49,7 @@ class MazeFormatter:
             maze += "\n"
         return maze, white_cells, black_cells, thick_walls, thin_walls
 
-    def calculate_cell_value(self, i, j):
+    def calculate_cell_value(self, i: int, j: int) -> (int, int):
         val = int(
             self.img[
                 i * self.m : (i + 1) * self.m, j * self.m : (j + 1) * self.m
@@ -59,15 +58,16 @@ class MazeFormatter:
         cell = 1 if val > 127 else 0
         return val, cell
 
-    @staticmethod
-    def update_cell_counts(cell, white_cells, black_cells):
+    def update_cell_counts(
+        self, cell: int, white_cells: int, black_cells: int
+    ) -> (int, int):
         if cell == 1:
             white_cells += 1
         else:
             black_cells += 1
         return white_cells, black_cells
 
-    def calculate_larger_area_value(self, i, j):
+    def calculate_larger_area_value(self, i: int, j: int) -> int:
         return int(
             self.img[
                 max(0, (i - 1) * self.m) : min((i + 2) * self.m, self.img.shape[0]),
@@ -75,31 +75,43 @@ class MazeFormatter:
             ].mean()
         )
 
-    @staticmethod
-    def update_wall_counts(larger_area_val, thick_walls, thin_walls):
+    def update_wall_counts(
+        self, larger_area_val: int, thick_walls: int, thin_walls: int
+    ) -> (int, int):
         if larger_area_val < 127:
             thick_walls += 1
         else:
             thin_walls += 1
         return thick_walls, thin_walls
 
-    def adjust_maze_colors(self, maze, white_cells, black_cells, wall_thickness):
+    def adjust_maze_colors(
+        self, maze: str, white_cells: int, black_cells: int, wall_thickness: int
+    ) -> str:
         if white_cells > black_cells:
             maze = self.swap_colors(maze)
         if wall_thickness == "thick":
             maze = self.swap_colors(maze)
         return maze
 
-    @staticmethod
-    def swap_colors(maze):
+    def swap_colors(self, maze: str) -> str:
         return maze.replace("0", "2").replace("1", "0").replace("2", "1")
 
-    def show(self):
-        maze = self.create_maze_array()
+    def show(self, border: bool = True):
+        maze = self.create_maze_array(border)
         maze_resized = self.resize_maze(maze)
         self.display_maze(maze_resized)
 
-    def create_maze_array(self):
+    def create_maze_array(self, border: bool) -> np.uint8:
+        if border:
+            maze = np.array(
+                [
+                    list(map(int, x.strip().split(" ")))
+                    for x in self.convert().split("\n")
+                    if x
+                ]
+            )
+            return self.add_border(maze)
+
         return np.array(
             [
                 list(map(int, x.strip().split(" ")))
@@ -108,21 +120,30 @@ class MazeFormatter:
             ]
         )
 
-    @staticmethod
-    def resize_maze(maze):
+    def resize_maze(self, maze: np.uint8):
         maze_resized = np.repeat(maze, 15, axis=0)
         maze_resized = np.repeat(maze_resized, 15, axis=1)
         maze_resized = maze_resized.astype(np.uint8) * 255  # 0 is black, 255 is white
         return 255 - maze_resized
 
-    @staticmethod
-    def display_maze(maze_resized):
+    def display_maze(self, maze_resized):
         cv2.imshow("Maze Preview", maze_resized)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+    def add_border(self, maze: np.uint8) -> np.uint8:
+        x = 1
+        y = self.N - 1
+        maze[0] = [1 for i in maze[0]]
+        maze[-1] = [1 for i in maze[-1]]
+        for i in maze:
+            i[0] = 1
+            i[-1] = 1
+
+        return maze
 
 
 if __name__ == "__main__":
     rm = MazeFormatter(IMG_PATH, N, M)
     print(rm.convert())
-    rm.show()
+    rm.show(border=True)
